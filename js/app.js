e7e4c7fd4680743573ed8c6d557f93afc8f49ee1
Marleyman876 <grant_garfield@yahoo.com>
2021-02-17 00:04:26 -0800

    added chart to JS

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@
 let sumClicks = 0;
 let allowedClicks = 25;
 let allProducts = [];
+let indexArray = [];
+let imageCount = 6;
 let productOne = document.querySelector('section img:first-child');
 let productTwo = document.querySelector('section img:nth-child(2)');
 let productThree = document.querySelector('section img:nth-child(3)');
@@ -47,49 +49,48 @@ new Product('wine-glass');
 function getRandomProducts() {
   return Math.floor(Math.random() * allProducts.length);
 }
+//new steps 
+
+
 
 function renderProducts() {
-  let indexArray = []
-  indexArray[0] = getRandomProducts();
-  indexArray[1] = getRandomProducts();
-  indexArray[2] = getRandomProducts();
-
-  while (indexArray[0] === indexArray[1]) {
-    indexArray[1] = getRandomProducts();
-    console.log('duplicated item')
+  while (indexArray.length < imageCount) {
+    let randomProducts = getRandomProducts();
+    while (!indexArray.includes(randomProducts)) {
+      indexArray.push(randomProducts)
+    }
   }
-  while (indexArray[1] === indexArray[2]) {
-    indexArray[2] = getRandomProducts();
-  }
-  while (indexArray[2] === indexArray[0]) {
-    indexArray[0] = getRandomProducts();
-  }
+  console.log(indexArray);
 
-  productOne.src = allProducts[indexArray[0]].src;
-  productOne.title = allProducts[indexArray[0]].name;
-  allProducts[indexArray[0]].views++;
+  let productOneIndex = indexArray.shift();
+  let productTwoIndex = indexArray.shift();
+  let productThreeIndex = indexArray.shift();
 
-  productTwo.src = allProducts[indexArray[1]].src;
-  productTwo.title = allProducts[indexArray[1]].name;
-  allProducts[indexArray[1]].views++;
+  productOne.src = allProducts[productOneIndex].src;
+  productOne.title = allProducts[productOneIndex].name;
+  allProducts[productOneIndex].views++;
 
-  productThree.src = allProducts[indexArray[2]].src;
-  productOne.title = allProducts[indexArray[2]].name;
-  allProducts[indexArray[2]].views++;
+  productTwo.src = allProducts[productTwoIndex].src;
+  productTwo.title = allProducts[productTwoIndex].name;
+  allProducts[productTwoIndex].views++;
+
+  productThree.src = allProducts[productThreeIndex].src;
+  productThree.title = allProducts[productThreeIndex].name;
+  allProducts[productThreeIndex].views++;
 }
 
 //render function 
 
-function renderClickResults() {
-  let myClicks = document.querySelector('ul');
-  for (let i = 0; i < allProducts.length; i++) {
-    let li = document.createElement('li');
-    li.textContent = `${allProducts[i].name} had ${allProducts[i].clicks} votes, and was seen ${allProducts[i].views} times`;
-    myClicks.appendChild(li);
+// function renderClickResults() {
+//   let myClicks = document.querySelector('ul');
+//   for (let i = 0; i < allProducts.length; i++) {
+//     let li = document.createElement('li');
+//     li.textContent = `${allProducts[i].name} had ${allProducts[i].clicks} votes, and was seen ${allProducts[i].views} times`;
+//     myClicks.appendChild(li);
 
-  }
+//   }
 
-}
+// }
 
 function clickHandler(event) {
   if (event.target === section) {
@@ -107,17 +108,68 @@ function clickHandler(event) {
   renderProducts();
   if (sumClicks === allowedClicks) {
     section.removeEventListener('click', clickHandler);
-
+    renderChart();
   }
 }
+
+
 function buttonClick(event) {
   if (sumClicks === allowedClicks) {
-    renderClickResults();
+    
   }
 }
+section.addEventListener('click', clickHandler);
 
 renderProducts();
 
-section.addEventListener('click', clickHandler);
 
-buttonResult.addEventListener('click', renderClickResults);
+
+//render chart 
+
+function renderChart(){
+  let productNames = [];
+  let productViews = [];
+  let productClicks = [];
+  for (let i = 0; i < allProducts.length; i++) {
+    productNames.push(allProducts[i].name);
+    productViews.push(allProducts[i].views);
+    productClicks.push(allProducts[i].clicks);
+  }
+  let chartObject = {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: 'Views',
+        data: productViews,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+      {
+        label: 'Clicks',
+        data: productClicks,
+        backgroundColor: 'blue',
+        borderColor: 'blue',
+        borderWidth: 1,
+      }],
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            stepSize: 1,
+          },
+        }],
+      },
+    },
+  };
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, chartObject);
+}
+
+renderChart();
+
+
+//buttonResult.addEventListener('click', renderClickResults);
